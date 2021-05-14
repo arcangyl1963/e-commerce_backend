755031d95278d3ad630c159f0f8bc92387858478
Jim Harris <arcangyl@gmail.com>
2021-05-13 22:53:33 -0400

    Fixed a syntax error in the connection script

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,15 @@
-const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
+const Sequelize = require('sequelize');
 
-sequelize = new Sequelize(process.env.MYSQL_URL);
-
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
 
 module.exports = sequelize;
